6cd95a8dbb04d41a160fe50202ee84d6d75eb06f
MargitStar <sovenokbymargo@mail.ru>
2021-07-28 15:44:27 +0300

    Change connections

--- asyncio_client/apps.py
+++ asyncio_client/apps.py
@@ -5,13 +5,18 @@ import random
 from client import tcp_echo_client
 from config import read_config
 
-   
+async def run_server(port, host):
+    while True: 
+        await tcp_echo_client(port, host)
+        await asyncio.sleep(random.randint(1,5))
+
 async def main():
     config = read_config('config.json')
-
     logging.basicConfig(level=logging.DEBUG)
 
-    for connection in range(config['connections']):
-        await tcp_echo_client(config['port'], config['host'])
-        await asyncio.sleep(random.randint(1,5))
-    
+    tasks = [
+        asyncio.create_task(run_server(config['port'], config['host']))
+        for connection in range(config['connections'])
+    ]
+
+    await asyncio.wait(tasks)
