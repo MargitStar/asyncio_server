02c305103ac23ac2736ddf66415182b735b46f6a
MargitStar <sovenokbymargo@mail.ru>
2021-08-02 11:06:39 +0300

    Add client packets view

--- flask_application/application.py
+++ flask_application/application.py
@@ -8,3 +8,8 @@ app = Flask(__name__)
 def packets_list_view():
     packets = Packet.all()
     return render_template('all_packets.html', packets=packets)
+
+@app.route('/api/client/<client_id>/packet')
+def client_packets_view(client_id):
+    packets = Packet.filtered_by_client_id(client_id=client_id)
+    return render_template('all_packets.html', packets=packets)
