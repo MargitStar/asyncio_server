9ec244bbb541d4c8923e8d7db72c4e204f63cfac
MargitStar <sovenokbymargo@mail.ru>
2021-08-04 15:34:08 +0300

    Refactor

--- asyncio_client/client.py
+++ asyncio_client/client.py
@@ -1,25 +1,42 @@
 import asyncio
 import logging
 
-from random import randint
+from random import choice, randint
+from enum import Enum
 
-from asyncio_client.utils import (write_connection_number, write_sequence, close_connection, 
-    write_mp_start, write_mp_body, write_mp_end)
+from asyncio_client.utils import (DataPacket, MultipartPacket, close_connection, write_connection_number)
+
+
+class Types(str, Enum):
+    DATA = 'DataPacket'
+    MULTIPART = 'MultipartPacket'
+
+async def write_data_packet_sequence(writer):
+    packet_size = randint(0,256)
+    packet = DataPacket(packet_size, writer)
+    await packet.write_sequence()
+
+async def write_mutipart_packet_sequence(writer):
+    packets_amount, packet_size = randint(1,32) ,randint(0,256)
+    packet = MultipartPacket(packets_amount, packet_size, writer)
+    await packet.write_mp_start()
+    await packet.write_mp_body()
+    await packet.write_mp_end()
+
+async def write_client(writer, connection):
+    data_choice = choice([Types.DATA, Types.MULTIPART])
+    await write_connection_number(writer, connection)
+    if data_choice == Types.DATA:
+        await write_data_packet_sequence(writer)
+    elif data_choice == Types.MULTIPART:
+        await write_mutipart_packet_sequence(writer)
+
+
 
 async def tcp_echo_client(host, port, connection):
-    reader, writer = await asyncio.open_connection(
+    _, writer = await asyncio.open_connection(
         host, port)
 
-    choice = randint(1, 2)
-
-    await write_connection_number(writer, connection)
-    
-    if choice == 1:
-        await write_sequence(writer)
-    elif choice == 2:
-        packet_size = await write_mp_start(writer)
-        for _ in range(packet_size):
-            await write_mp_body(writer)
-        await write_mp_end(writer)
+    await write_client(writer, connection)
     await close_connection(writer)
     
--- asyncio_client/utils.py
+++ asyncio_client/utils.py
@@ -3,73 +3,64 @@ import logging
 from random import randbytes, randint
 
 def convert_str_to_bytes(string):
-    return f'{string}\n'.encode()
+    return string.encode()
 
 
 def convert_int_to_bytes(number):
     return bytes([number])
 
 
-async def generate_dp_packet(size):
-    sequence_type = 'DP'
-    begining = b'\xaa\xbb\xcc\xdd'
-    ending = b'\xdd\xcc\xbb\xaa'
-    sequence = begining + randbytes(size) + ending
-    return sequence, sequence_type
-
-
-async def generate_mp_start_packet(size):
-    return 'MPS', convert_int_to_bytes(size) + b'MPS'
-
-
-async def generate_mp_body_packet(size):
-    return 'MPB', randbytes(size) + b'MPB'
-
-
-async def generate_mp_end_packet():
-    return 'MPE', b'MPE'
-
-
-async def write_connection_number(writer, connection):
-    writer.write(convert_str_to_bytes(connection))
-
-
-async def get_sequence():
-    packet_size = randint(0,255)
-    sequence = await generate_dp_packet(packet_size)
-    return sequence
-
-
-async def write_mp_start(writer):
-    packets_amount = randint(1,20)
-    start_sequence_type, start_sequence = await generate_mp_start_packet(packets_amount)
-    writer.write(convert_str_to_bytes(start_sequence_type))
-    writer.write(start_sequence)
-    return packets_amount
-
-
-async def write_mp_body(writer):
-    packet_size = randint(0,255)
-    body_sequence_type, body_sequence = await generate_mp_body_packet(packet_size)
-    writer.write(convert_str_to_bytes(body_sequence_type))
-    writer.write(body_sequence)
-
-
-async def write_mp_end(writer):
-    end_sequence_type, end_sequence = await generate_mp_end_packet()
-    writer.write(convert_str_to_bytes(end_sequence_type))
-    writer.write(end_sequence)   
-    
-
-async def write_sequence(writer):
-    sequence, sequence_type = await get_sequence()
-    logging.info(f'Send: {sequence}')
-    writer.write(convert_str_to_bytes(sequence_type))
-    writer.write(sequence)
-    
-
 async def close_connection(writer):
     await writer.drain()
     logging.info('Connection closed!')
     writer.close()
     await writer.wait_closed()
+
+
+async def write_connection_number(writer, connection):
+    writer.write(convert_str_to_bytes(str(connection).zfill(4)))
+
+
+class DataPacket:
+    def __init__(self, packet_size, writer):
+        self.packet_size = packet_size
+        self.writer = writer
+
+    async def generate_dp_packet(self):
+        sequence_type = b'DTP'
+        sequence = sequence_type + randbytes(self.packet_size)
+        return sequence
+
+    async def write_sequence(self):
+        sequence = await self.generate_dp_packet()
+        logging.info(f'Send: {sequence}')
+        self.writer.write(sequence)
+
+
+class MultipartPacket:
+    def __init__(self, packets_amount, packet_size, writer):
+        self.packets_amount = packets_amount
+        self.packet_size = packet_size
+        self.writer = writer
+
+    async def generate_mp_start_packet(self):
+        return convert_int_to_bytes(self.packets_amount) + b'MPS'
+
+    async def generate_mp_body_packet(self):
+        return randbytes(self.packet_size) + b'MPB'
+
+    async def generate_mp_end_packet(self):
+        return b'MPE'
+
+    async def write_mp_start(self):
+        start_sequence = await self.generate_mp_start_packet()
+        self.writer.write(start_sequence)
+
+    async def write_mp_body(self):
+        for _ in range(self.packet_size):
+            body_sequence = await self.generate_mp_body_packet()
+            self.writer.write(body_sequence)
+
+    async def write_mp_end(self):
+        end_sequence = await self.generate_mp_end_packet()
+        self.writer.write(end_sequence)  
