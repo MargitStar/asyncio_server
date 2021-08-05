a1f8c409e827be30c29b1f9d92c0460050010854
MargitStar <sovenokbymargo@mail.ru>
2021-08-05 12:35:53 +0300

    Change DelType

--- asyncio_server/utils.py
+++ asyncio_server/utils.py
@@ -3,7 +3,6 @@ from datetime import datetime
 
 from asyncio_db.models import Packet, DataPacket, MultipartPacket
 
-
 class DelimiterType(bytes, Enum):
     INTERMEDIATE = b'MPB'
     END = b'MPE'
@@ -13,10 +12,8 @@ async def read_data(reader):
     data = await reader.read()
     return data
 
-
 def convert_data_to_string(connection):
     return connection.decode('utf-8')
-
 
 def convert_data_to_int(number):
     return int.from_bytes(number)
@@ -38,41 +35,25 @@ class DataPacketParser(DataParser):
         return self.data[7:]
 
     def write_to_db(self):
-        packet = Packet.add(
-            type=self.parse_data_type(),
-            timestamp=datetime.utcnow(),
-            client_id=self.parse_connection_number())
+        packet = Packet.add(type=self.parse_data_type(), timestamp=datetime.utcnow(), client_id=self.parse_connection_number())
         DataPacket.add(data=self.get_dtp_data(), packet=packet)
 
 
 class MultipartDataParser(DataParser):
     def parse_packet_amount(self):
         return int(convert_data_to_string(self.data[7:11]))
-
+    
     def parse_packets(self):
-        packet_data = self.data[11:].rstrip(
-            DelimiterType.END).split(DelimiterType.INTERMEDIATE)
+        packet_data = self.data[11:].rstrip(DelimiterType.END).split(DelimiterType.INTERMEDIATE)
         packet_data.remove(b'')
-        print(packet_data)
         return packet_data
-
+        
+    
     def write_to_db(self):
-        self.parse_packets()
         client_id = self.parse_connection_number()
-        start_packet = Packet.add(
-            type=self.parse_data_type(),
-            timestamp=datetime.utcnow(),
-            client_id=client_id)
+        start_packet = Packet.add(type=self.parse_data_type(), timestamp=datetime.utcnow(), client_id=client_id)
         packet_amount = self.parse_packet_amount()
         for _ in range(packet_amount):
-            Packet.add(
-                type=convert_data_to_string(
-                    DelimiterType.INTERMEDIATE),
-                timestamp=datetime.utcnow(),
-                client_id=client_id)
-        end_packet = Packet.add(
-            type=convert_data_to_string(
-                DelimiterType.END),
-            timestamp=datetime.utcnow(),
-            client_id=client_id)
+            Packet.add(type=convert_data_to_string(DelimiterType.INTERMEDIATE), timestamp=datetime.utcnow(), client_id=client_id)
+        end_packet = Packet.add(type=convert_data_to_string(DelimiterType.END), timestamp=datetime.utcnow(), client_id=client_id)
         MultipartPacket.add(start_packet=start_packet, end_packet=end_packet)
