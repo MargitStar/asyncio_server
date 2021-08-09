03f5701511706d55c22cf8a7241d9c966d30e79e
MargitStar <sovenokbymargo@mail.ru>
2021-08-09 15:11:58 +0300

    Add mp_full view

--- flask-application/flask_application/application.py
+++ flask-application/flask_application/application.py
@@ -21,6 +21,18 @@ def packets_list_mp_view():
     return jsonify([mp_data_schema.dump(mp_packet) for mp_packet in mp_packets])
 
 
+@app.route('/api/packets/mp-full/')
+def packets_mp_view():
+    result = {}
+    multipart_data = MultipartPacket.all()
+    mp_packet_id = [data.id for data in multipart_data]
+    for packet_id in mp_packet_id:
+        mp_packets = MultipartData.filtered_by_idx(packet_id)
+        packet = [mp_packet.data for mp_packet in mp_packets]
+        result[f'MP_packet_{packet_id}'] = ''.join(packet)
+    return result
+
+
 @app.route('/api/packets/mp-full/<int:mp_packet_id>')
 def packets_mp_detail_view(mp_packet_id):
     mp_packets = MultipartData.filtered_by_idx(mp_packet_id)
