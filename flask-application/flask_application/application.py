2545159244422f49c6dda41f8b834c275e10873a
MargitStar <sovenokbymargo@mail.ru>
2021-08-09 13:43:41 +0300

    Add multipart packet view

--- flask-application/flask_application/application.py
+++ flask-application/flask_application/application.py
@@ -1,15 +1,25 @@
 from flask import Flask, jsonify
 
-from asyncio_db.models import Packet, DataPacket
-from flask_application.utils import PacketSchema, DataPacketSchema
+from asyncio_db.models import Packet, DataPacket, MultipartData
+from flask_application.utils import PacketSchema, DataPacketSchema, MultipartDataSchema
+
 
 app = Flask(__name__)
 
-@app.route('/api/dtp/packet')
+
+@app.route('/api/dtp/packets')
 def packets_list_dtp_view():
     data_packets = DataPacket.all()
     data_schema = DataPacketSchema()
     return jsonify([data_schema.dump(data_packet) for data_packet in data_packets])
+
+
+@app.route('/api/mp/packets')
+def packets_list_mp_view():
+    mp_packets = MultipartData.all()
+    mp_data_schema = MultipartDataSchema()
+    return jsonify([mp_data_schema.dump(mp_packet) for mp_packet in mp_packets])
+
 
 @app.route('/api/client/<client_id>/packet')
 def client_packets_view(client_id):
--- flask-application/flask_application/utils.py
+++ flask-application/flask_application/utils.py
@@ -1,5 +1,6 @@
 from marshmallow import Schema, fields
 
+
 class PacketSchema(Schema):
     id = fields.Int()
     type = fields.Str()
@@ -11,3 +12,17 @@ class DataPacketSchema(Schema):
     id = fields.Int()
     data_packet = fields.Nested(PacketSchema)
     data = fields.Str()
+
+
+class MultipartPacketScema(Schema):
+    id = fields.Int()
+    start_packet_id = fields.Int()
+    end_packet_id = fields.Int()
+
+
+class MultipartDataSchema(Schema):
+    id = fields.Int()
+    data = fields.Str()
+    idx = fields.Int()
+    packet = fields.Nested(PacketSchema)
+    mp_packet = fields.Nested(MultipartPacketScema)
